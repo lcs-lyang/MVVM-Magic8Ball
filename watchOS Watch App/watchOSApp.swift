0be71c358544e6c13119351255768fecf5d20198
Lillian Yang <lyang@lcs.on.ca>
2023-01-29 23:00:33 -0500

    Finished watchOS interface.

---
+++ watchOS Watch App/watchOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchOSApp.swift
+//  watchOS Watch App
+//
+//  Created by Lillian Yang on 2023-01-29.
+//
+
+import SwiftUI
+
+@main
+struct watchOS_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

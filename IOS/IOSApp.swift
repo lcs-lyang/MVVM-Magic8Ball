396f629018e3bea4fef8da3916d6f16b16d2474f
Lillian Yang <lyang@lcs.on.ca>
2023-01-29 22:27:03 -0500

    Initial Commit

---
+++ IOS/IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSApp.swift
+//  IOS
+//
+//  Created by Lillian Yang on 2023-01-29.
+//
+
+import SwiftUI
+
+@main
+struct IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
